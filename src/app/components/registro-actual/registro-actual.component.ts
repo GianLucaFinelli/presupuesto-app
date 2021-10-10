17715257d05e298914962f3a6587f6d55abf3424
Gian Luca Finelli <gianfinelli@gmail.com>
2021-10-10 19:09:15 -0200

    [Figma] [UI] Shadows

--- src/app/components/registro-actual/registro-actual.component.ts
+++ src/app/components/registro-actual/registro-actual.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CotizadorPrespuestoService } from 'src/app/services/cotizador-prespuesto.service';
+import { Cotizacion } from 'src/app/shared/models/cotizacion';
 
 @Component({
   selector: 'app-registro-actual',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroActualComponent implements OnInit {
 
-  constructor() { }
+  result : boolean = true;
+  cotizacion: Cotizacion = new Cotizacion({});
+  cuotas: number[] = [];
+  precio: number = 0;
 
+  constructor(private cotizadorService: CotizadorPrespuestoService) { }
   ngOnInit(): void {
-  }
+    this.cotizadorService.resultCotizacion.subscribe( (next: boolean) => {
+      this.result = next;
+    })
 
+    this.cotizadorService.cotizacion.subscribe( (next: Cotizacion) => {
+      this.cotizacion = next;
+      console.log(this.cotizacion.Cuotas(this.cotizacion));
+      this.cuotas = this.cotizacion.Cuotas(this.cotizacion);
+      this.precio = 0;
+      this.cuotas.map( cuota => {
+        this.precio += cuota;
+      })
+    })
+  }
 }
